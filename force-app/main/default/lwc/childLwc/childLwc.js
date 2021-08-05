0854b06e60db0795caa468db40faae0ea76bbfbb
Lakshmi Deepak Inkurthi <i.lakshmideepak@gmail.com>
2021-08-05 06:27:45 +0530

    rules for events

--- force-app/main/default/lwc/childLwc/childLwc.js
+++ force-app/main/default/lwc/childLwc/childLwc.js
@@ -8,6 +8,12 @@ export default class ChildLwc extends LightningElement {
         alert("Your Message is :"+valueStr);
     }
     customEventDispatch(event){
+        /*
+        --> No Upper Case in event name
+        --> No Space in event Name
+        --> No special characters in event Name 
+        --> NO Underscore to seperate words in event Name
+         */
         let eventName = 'customeventexample';
         let data = {firstName : "Deepak" , lastName : "Lakshmi"};
         let eventDetails = new CustomEvent(eventName , {detail : data});
